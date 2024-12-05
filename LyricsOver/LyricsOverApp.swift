aa286a05db52db140eea50e2957d3b237312f35d
alexstrnik <alex.str.nik@gmail.com>
2024-12-06 01:25:14 +0300

    Initial Commit

---
+++ LyricsOver/LyricsOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsOverApp.swift
+//  LyricsOver
+//
+//  Created by Aleksandr Strizhnev on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LyricsOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
